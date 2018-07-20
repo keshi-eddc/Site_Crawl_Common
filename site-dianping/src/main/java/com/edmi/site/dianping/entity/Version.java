fabaacc4c77bb7c7388eb7b54e543b61da72e8dc
conner.qin <conner.qin@earlydata.com>
2018-07-20 15:52:40 +0800

    删除site-dianping模块

--- site-dianping/src/main/java/com/edmi/site/dianping/entity/Version.java
+++
@@ -1,9 +0,0 @@
-package com.edmi.site.dianping.entity;
-
-public class Version {
-	
-	public static final String version = "init";
-	
-	public static final String version_last_month = "201806";
-	
-}
